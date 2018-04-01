d40dd9dc63f1e556348f60c1e190eafa207fcad4
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2018-04-01 17:31:27 +0200

    First version of spreadscript.

---
+++ spreadscript/__init__.py
@@ -0,0 +1,27 @@
+"""spreadscript: Use a spreadsheet as a function.
+
+
+Copyright (c) 2018 Jeroen F.J. Laros <jlaros@fixedpoint.nl>
+
+Licensed under the MIT license, see the LICENSE file.
+"""
+from .spreadscript import SpreadScript
+
+
+__version_info__ = ('0', '0', '1')
+
+__version__ = '.'.join(__version_info__)
+__author__ = 'Jeroen F.J. Laros'
+__contact__ = 'jlaros@fixedpoint.nl'
+__homepage__ = 'https://github.com/jfjlaros/spreadscript'
+
+usage = __doc__.split('\n\n\n')
+
+
+def doc_split(func):
+    return func.__doc__.split('\n\n')[0]
+
+
+def version(name):
+    return '{} version {}\n\nAuthor   : {} <{}>\nHomepage : {}'.format(
+        name, __version__, __author__, __contact__, __homepage__)
---
+++ spreadscript/spreadscript.py
@@ -0,0 +1,111 @@
+import os
+import subprocess
+
+import uno
+import unohelper
+from com.sun.star.connection import NoConnectException
+
+
+class SpreadScript(object):
+    def __init__(self):
+        """Initialise the class."""
+        self._desktop = None
+        self._start_soffice()
+        self._connect_soffice()
+
+    def __del__(self):
+        """Close the soffice instance."""
+        if self._desktop:
+            self.close()
+
+    def _start_soffice(self):
+        """Start soffice in the background."""
+        process_id = os.fork()
+        if not process_id:
+            subprocess.call(
+                'soffice --accept="socket,host=localhost,port=2002;urp;" ' + 
+                '--norestore --nologo --nodefault --headless', shell=True)
+            exit()
+
+    def _connect_soffice(self):
+        """Connect to a running soffice instance."""
+        local_context = uno.getComponentContext()
+        resolver = local_context.ServiceManager.createInstanceWithContext(
+            'com.sun.star.bridge.UnoUrlResolver', local_context)
+        while True:
+            try:
+                component_context = resolver.resolve(
+                    'uno:socket,host=localhost,port=2002;urp;' +
+                    'StarOffice.ComponentContext')
+            except NoConnectException:
+                pass
+            else:
+                break
+        smgr = component_context.ServiceManager
+        self._desktop = smgr.createInstanceWithContext(
+            'com.sun.star.frame.Desktop', component_context)
+
+    def _get_cell_text(self, column, row):
+        return self._interface.getCellByPosition(column, row).getString()
+
+    def _get_cell_value(self, column, row):
+        return self._interface.getCellByPosition(column, row).getValue()
+
+    def _set_cell_value(self, column, row, value):
+        self._interface.getCellByPosition(column, row).setValue(value)
+
+    def _read_table(self, column):
+        """Read names and values from a table.
+
+        :arg int column: Upper-left coordinate of the table content.
+
+        :returns dict: Table content.
+        """
+        inputs = {}
+
+        row = 3
+        while True:
+            name = self._get_cell_text(column, row)
+            value = self._get_cell_value(column + 1, row)
+            if not name:
+                break
+            inputs[name] = value
+            row += 1
+
+        return inputs
+
+    def _write_table(self, column, data):
+        """Write values to a table.
+
+        :arg int column: Upper-left coordinate of the table content.
+        :arg dict data: Data to be written.
+        """
+        row = 3
+        while True:
+            name = self._get_cell_text(column, row)
+            if not name:
+                break
+            self._set_cell_value(column + 1, row, data[name])
+            row += 1
+
+    def open(self, file_name):
+        """Open a spreadsheet.
+
+        :arg str file_name: File name.
+        """
+        doc_url = unohelper.systemPathToFileUrl(file_name)
+        self._desktop.loadComponentFromURL(doc_url, '_blank', 0, ())
+        self._interface = self._desktop.getCurrentComponent().Sheets.Interface
+
+    def close(self):
+        """Close the soffice instance."""
+        self._desktop.terminate()
+
+    def read_input(self):
+        return self._read_table(1)
+
+    def write_input(self, data):
+        self._write_table(1, data)
+
+    def read_output(self):
+        return self._read_table(4)
